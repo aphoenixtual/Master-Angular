71a040705380f7c7ca5eb49c798ded3e9a8f7a10
Aayush <ayushp264@gmail.com>
2023-01-07 00:27:10 +0530

    EventBindingSyntax

---
+++ EventbindingSyntax/src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: ` <div class="app">
+    <h1 (click)="handleClick($event)">{{ message }}</h1>
+    <h1 (click)="handleClick($event)">{{ newMessage }}</h1>
+    <input value="123">
+    <input [value]="message" (input)="handleInput($event)">
+  </div> `,
+  styles: [
+    `
+      .app {
+        margin-top: 50px;
+        font-size: 22px;
+        color: #fff;
+        text-align: center;
+      }
+    `,
+  ],
+})
+export class AppComponent implements OnInit {
+  message!: string;
+  newMessage!: string;
+
+  ngOnInit() {
+    this.message = 'Heyyy Aayush'
+  }
+
+  handleClick(event: Event) {
+    console.log('Clicked')
+    console.log(event)
+  }
+
+  handleInput(event: Event){
+    console.log(event.target) //this is going to give the element itself
+    const { value } = event.target as HTMLInputElement; // This is type casting in typescript
+    // here we can obtain our value by referencing our event.target here we're using some destructuring so we can pull our property out
+    console.log(value)
+    this.message = value;
+    this.newMessage = value;
+  }
+}
