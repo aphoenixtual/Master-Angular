5369df691e8e3c79c004eef1d9c292bc4a79ab3a
Aayush <ayushp264@gmail.com>
2023-01-07 02:14:13 +0530

    create-a-smart-container-component

---
+++ 10-create-a-smart-container-component/src/app/admin/containers/donut-list/donut-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-donut-list',
+  template: `
+    <p>
+      donut-list works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class DonutListComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+}
