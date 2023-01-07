d0d58a8e1bc74864f6e689504251673366c2b718
Aayush <ayushp264@gmail.com>
2023-01-07 18:28:50 +0530

    type-definitions-with-interfaces

---
+++ 12-type-definitions-with-interfaces/src/app/admin/containers/donut-list/donut-list.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { Donut } from '../../models/donut.model';
+
+@Component({
+  selector: 'app-donut-list',
+  template: `
+    <div>
+        <div>
+            {{ donut.name }}
+            {{ donut.price }}
+        </div>
+
+        <div>
+            {{ donut | json }}  <!--Json pipe inline one of the Angular way to implement pipe-->
+        </div>
+    </div>
+  `,
+  styles: [
+  ]
+})
+export class DonutListComponent implements OnInit {
+    donut!: Donut;
+    donuts!: Donut[];
+    constructor() {}
+    ngOnInit(): void {
+        this.donuts = [
+            {
+                id: 'y8z0As',
+                name: 'Just-Chocolate',
+                icon: 'just-chocolate',
+                price: 119,
+                description: 'For the pure chocoholic.'
+            },
+            {
+                id: '3u98K1',
+                name: 'Glazed Fudge',
+                icon: 'glazed-fudge',
+                price: 129,
+                description: 'Sticky perfection.'
+            },
+            {
+                id: 'ae098s',
+                name: 'Caramel Swirl',
+                icon: 'caramel-swirl',
+                price: 129,
+                description: 'Chocolate drizzled with caramel.'
+            }
+        ]
+
+        this.donut = this.donuts[0];
+       // this.donut = JSON.stringify(this.donuts[0]); // JS way of implement pipe
+    }
+
+}
