07bae0e30b989862464f5390880e41265d4bb444
Aayush <ayushp264@gmail.com>
2023-01-25 08:09:08 +0530

    use-ng-if-for-conditional-rendering

---
+++ 23-use-ng-if-for-conditional-rendering/src/app/admin/containers/donut-list/donut-list.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { Donut } from '../../models/donut.model';
+
+@Component({
+  selector: 'app-donut-list',
+  template: `
+  <div>
+    <div *ngIf="donuts.length">
+        <app-donut-card [donut]="donuts[0]"></app-donut-card> <!-- This is hard coded not very ideal way of doing things-->
+        <app-donut-card [donut]="donuts[1]"></app-donut-card>
+        <app-donut-card [donut]="donuts[2]"></app-donut-card>
+    </div>
+  </div>
+  `,
+  styles: [
+  ]
+})
+export class DonutListComponent implements OnInit {
+    donuts!: Donut[];
+    constructor() {}
+    ngOnInit(): void {
+        this.donuts = [
+            {
+                id: 'y8z0As',
+                name: 'Just-Chocolate',
+                icon: 'just-chocolate',
+                price: 119,
+                description: 'For the pure chocoholic.'
+            },
+            {
+                id: '3u98K1',
+                name: 'Glazed Fudge',
+                icon: 'glazed-fudge',
+                price: 129,
+                promo: true,
+                description: 'Sticky perfection.'
+            },
+            {
+                id: 'ae098s',
+                name: 'Caramel Swirl',
+                icon: 'caramel-swirl',
+                price: 129,
+                description: 'Chocolate drizzled with caramel.'
+            },
+        ];
+    }
+
+}
