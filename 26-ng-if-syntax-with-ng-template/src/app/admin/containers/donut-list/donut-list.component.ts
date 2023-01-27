bc10eea0d514c03e6b03a9845921aed18147e0fd
Aayush <ayushp264@gmail.com>
2023-01-28 01:18:19 +0530

    ng-if-syntax-with-ng-template

---
+++ 26-ng-if-syntax-with-ng-template/src/app/admin/containers/donut-list/donut-list.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { Donut } from '../../models/donut.model';
+
+@Component({
+  selector: 'app-donut-list',
+  template: `
+  <div>
+    <ng-container *ngIf="donuts.length; else nothing"> <!-- This is the first way to use ngIf as a container for grouping-->
+        <app-donut-card [donut]="donuts[0]"></app-donut-card> 
+        <app-donut-card [donut]="donuts[1]"></app-donut-card>
+        <app-donut-card [donut]="donuts[2]"></app-donut-card>
+    </ng-container>
+
+    <ng-template [ngIf]="donuts.length" [ngIfElse]="nothing"]><!-- This is the other way to use ngIf as a template for advance and complex use-->
+        <app-donut-card [donut]="donuts[0]"></app-donut-card> 
+        <app-donut-card [donut]="donuts[1]"></app-donut-card>
+        <app-donut-card [donut]="donuts[2]"></app-donut-card>
+    </ng-template>
+
+    <ng-template #nothing>
+        <p>No Donuts here...</p>
+    </ng-template>
+
+  </div>
+  `,
+  styles: [
+  ]
+})
+export class DonutListComponent implements OnInit {
+    donuts!: Donut[];
+    constructor() {}
+    ngOnInit(): void {
+        this.donuts = [
+            {
+                id: 'y8z0As',
+                name: 'Just-Chocolate',
+                icon: 'just-chocolate',
+                price: 119,
+                description: 'For the pure chocoholic.'
+            },
+            {
+                id: '3u98K1',
+                name: 'Glazed Fudge',
+                icon: 'glazed-fudge',
+                price: 129,
+                promo: true,
+                description: 'Sticky perfection.'
+            },
+            {
+                id: 'ae098s',
+                name: 'Caramel Swirl',
+                icon: 'caramel-swirl',
+                price: 129,
+                description: 'Chocolate drizzled with caramel.'
+            },
+        ];
+    }
+
+}
