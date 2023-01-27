86a4aeb909b8f390f891a51609c1206ef080e0e9
Aayush <ayushp264@gmail.com>
2023-01-28 03:52:56 +0530

    Input-binding-with-ng-model

---
+++ 35-input-binding-with-ng-model/src/app/admin/containers/donut-list/donut-list.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import { Donut } from '../../models/donut.model';
+
+@Component({
+  selector: 'app-donut-list',
+  template: `
+  <div>
+    <ng-container *ngIf="donuts.length; else nothing"> <!-- This is the first way to use ngIf as a container for grouping-->
+
+        <app-donut-card 
+            *ngFor="let donut of donuts; trackBy: trackById" 
+            [donut]="donut">
+        </app-donut-card>
+
+    </ng-container>
+
+    <ng-template #nothing>
+        <p>No Donuts here...</p>
+    </ng-template>
+
+  </div>
+  `,
+  styles: [
+  ]
+})
+export class DonutListComponent implements OnInit {
+    donuts!: Donut[];
+    constructor() {}
+    ngOnInit(): void {
+        this.donuts = [
+            {
+                id: 'y8z0As',
+                name: 'Just-Chocolate',
+                icon: 'just-chocolate',
+                price: 119,
+                promo: 'limited',
+                description: 'For the pure chocoholic.'
+            },
+            {
+                id: '3u98K1',
+                name: 'Glazed Fudge',
+                icon: 'glazed-fudge',
+                price: 129,
+                promo: 'new',
+                description: 'Sticky perfection.'
+            },
+            {
+                id: 'ae098s',
+                name: 'Caramel Swirl',
+                icon: 'caramel-swirl',
+                price: 139,
+                description: 'Chocolate drizzled with caramel.'
+            },
+            {
+                id: '8amz94',
+                name: 'Sour Supreme',
+                icon: 'sour-supreme',
+                price: 149,
+                description: 'For the sour advocate.'
+            },
+            {
+                id: 'l3Mno',
+                name: 'Zesty Lemon',
+                icon: 'zesty-lemon',
+                price: 189,
+                description: 'Delicious lucious lemon.'
+            },
+        ];
+    }
+
+    trackById(index: number, value: Donut) {
+        return value.id;
+    }
+
+}
