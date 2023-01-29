68f9003ad097b1e6ac801cb878c90de7fe9cb1a2
Aayush <ayushp264@gmail.com>
2023-01-30 04:20:36 +0530

    Class-methods-as-state-selectors

---
+++ 53-class-methods-as-state-selectors/src/app/admin/containers/donut-single/donut-single.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Donut } from '../../models/donut.model';
+import { DonutService } from '../../services/donut.service';
+
+@Component({
+  selector: 'donut-single',
+  template: `
+   <div>
+    <donut-form [donut]="donut" (create)="onCreate($event)"></donut-form>
+   </div>
+  `,
+  styles: [
+  ]
+})
+export class DonutSingleComponent implements OnInit {
+    
+    donut!: Donut;
+
+
+  constructor(private donutService: DonutService) { }
+
+  ngOnInit(): void {
+    //CRUD
+    this.donut = this.donutService.readOne('l3Mno')
+  }
+
+  onCreate(donut: Donut) {
+    console.log('onCreate', donut);
+  }
+
+}
