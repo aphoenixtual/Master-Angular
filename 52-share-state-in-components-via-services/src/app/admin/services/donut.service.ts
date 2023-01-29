e21e798e42223c6efbb2f1ccfd3ae64a6472bb03
Aayush <ayushp264@gmail.com>
2023-01-30 03:56:25 +0530

    Share-state-in-components-via-services

---
+++ 52-share-state-in-components-via-services/src/app/admin/services/donut.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { Donut } from '../models/donut.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DonutService {
+
+    donuts: Donut[] = [
+        {
+            id: 'y8z0As',
+            name: 'Just-Chocolate',
+            icon: 'just-chocolate',
+            price: 119,
+            promo: 'limited',
+            description: 'For the pure chocoholic.'
+        },
+        {
+            id: '3u98K1',
+            name: 'Glazed Fudge',
+            icon: 'glazed-fudge',
+            price: 129,
+            promo: 'new',
+            description: 'Sticky perfection.'
+        },
+        {
+            id: 'ae098s',
+            name: 'Caramel Swirl',
+            icon: 'caramel-swirl',
+            price: 139,
+            description: 'Chocolate drizzled with caramel.'
+        },
+        {
+            id: '8amz94',
+            name: 'Sour Supreme',
+            icon: 'sour-supreme',
+            price: 149,
+            description: 'For the sour advocate.'
+        },
+        {
+            id: 'l3Mno',
+            name: 'Zesty Lemon',
+            icon: 'zesty-lemon',
+            price: 189,
+            description: 'Delicious lucious lemon.'
+        },
+    ];
+
+  constructor() { }
+}
