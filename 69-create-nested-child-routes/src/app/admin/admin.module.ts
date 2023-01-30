9906e90ea6191acaf6af57ee8d6571b6c853852a
Aayush <ayushp264@gmail.com>
2023-01-30 23:24:50 +0530

    Create-nested-child-routes

---
+++ 69-create-nested-child-routes/src/app/admin/admin.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+// containers
+import { DonutListComponent } from './containers/donut-list/donut-list.component';
+import { DonutSingleComponent } from './containers/donut-single/donut-single.component';
+
+// components
+import { DonutCardComponent } from './components/donut-card/donut-card.component';
+import { DonutFormComponent } from './components/donut-form/donut-form.component';
+
+
+// services
+
+// guards
+
+// directives
+@NgModule({
+  declarations: [
+    DonutListComponent,
+    DonutSingleComponent,
+    DonutCardComponent,
+    DonutFormComponent
+  ],
+  imports: [ CommonModule, FormsModule, HttpClientModule],
+  exports: [DonutListComponent, DonutSingleComponent]
+})
+export class AdminModule { }
