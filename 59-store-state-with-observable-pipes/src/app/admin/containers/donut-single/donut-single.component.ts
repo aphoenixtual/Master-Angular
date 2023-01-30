c316e09fc1930cdc14e6e01b52221b0ace8ece2e
Aayush <ayushp264@gmail.com>
2023-01-30 19:19:35 +0530

    Store-state-with-observable-pipes

---
+++ 59-store-state-with-observable-pipes/src/app/admin/containers/donut-single/donut-single.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Donut } from '../../models/donut.model';
+import { DonutService } from '../../services/donut.service';
+
+@Component({
+  selector: 'donut-single',
+  template: `
+   <div>
+    <donut-form 
+        [donut]="donut"
+        (create)="onCreate($event)"
+        (update)="onUpdate($event)"
+        (delete)="onDelete($event)"
+    ></donut-form>
+   </div>
+  `,
+  styles: [
+  ]
+})
+export class DonutSingleComponent implements OnInit {
+    
+    donut!: Donut;
+
+
+  constructor(private donutService: DonutService) { }
+
+  ngOnInit(): void {
+    //CRUD
+    //donutService - Smart contianer
+    //this.donut = this.donutService.readOne('l3Mno')
+  }
+
+  onCreate(donut: Donut) {
+    this.donutService.create(donut);
+  }
+
+  onUpdate(donut: Donut) {
+    this.donutService.update(donut);
+  }
+
+  onDelete(donut: Donut) {
+    this.donutService.delete(donut);
+  }
+
+}
